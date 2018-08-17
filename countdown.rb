d7dae72284bd6aef305bfaeb649f53324d735eea
braj011 <bhagyaraj01@gmail.com>
2018-08-17 11:52:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,11 +10,11 @@ end
 
 
 
-def countdown_with_sleep(number)
-  while number > 0
+def countdown_with_sleep(int)
+  while int > 0
    sleep 1
-    puts "#{number} SECOND(S)!"
-    number -= 1
+    puts "#{int} SECOND(S)!"
+    int -= 1
   end
   return "HAPPY NEW YEAR!"
 end
